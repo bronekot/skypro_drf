fa1d1d4d26a9cb3f281552f0e757b470634a2c0e
BroneKot <13498924+bronekot@users.noreply.github.com>
2024-09-16 12:52:09 +0300

    Update serializers.py

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,10 +4,11 @@ from .models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
-        fields = ["id", "title", "preview", "description", "lesson_count"]
+        fields = ["id", "title", "preview", "description", "lesson_count", "lessons"]
 
     def get_lesson_count(self, obj):
         return obj.lessons.count()
